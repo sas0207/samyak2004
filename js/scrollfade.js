495662f25f8fdc545e9b344c2b3399da6a803533
sash0207 <samyakaug2004@gmail.com>
2025-09-20 19:23:30 +0530

    Update scrollfade.js

--- js/scrollfade.js
+++ js/scrollfade.js
@@ -1,17 +1,1 @@
-window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
-  const sparrow = document.querySelector('.sparrow-img');
-  const intro = document.querySelector('.sparrow-intro');
-  const siteContent = document.getElementById('site-content');
-  const maxScroll = document.body.scrollHeight - window.innerHeight;
 
-  // Shimmer movement only
-  const shimmerShift = scrollY % 200;
-  sparrow.style.backgroundPosition = `${shimmerShift}px ${shimmerShift}px`;
-
-  // Fade out bird and reveal site at bottom
-  if (scrollY >= maxScroll - 50) {
-    intro.style.opacity = 0;
-    siteContent.style.opacity = 1;
-  }
-});
